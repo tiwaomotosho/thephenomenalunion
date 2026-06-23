6fae91a252ba7246afaaf222dbb7680f293fa733
Tiwalade Omotosho <tiwababa194@gmail.com>
2026-06-23 23:38:42 +0100

    add server updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    tsConfigPaths(),
+    tsconfigPaths(),
     tailwindcss(),
-    tanstackStart({
-      server: {
-        entry: "server",
-        preset: "vercel",
-      },
-    }),
+    tanstackStart(),
+    nitro({ preset: "vercel" }),
+    viteReact(),
   ],
 });
